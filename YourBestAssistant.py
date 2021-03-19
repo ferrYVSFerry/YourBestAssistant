06269aa7ee207f6633d10894ee1dba17f7e3a5f1
werisme <marcosalza0@gmail.com>
2021-03-19 10:04:09 +0100

    Update YourBestAssistant.py
    a

--- YourBestAssistant.py
+++ YourBestAssistant.py
@@ -1,3 +1,5 @@
+from tkinter import END
+
 from PyQt5 import QtCore, QtWidgets
 from datetime import datetime
 import subprocess
@@ -22,7 +24,62 @@ def tts(text):
     TTS.tts_it(text)
 
 
+def ScrapingRequest(self, text, current_time):
+    my_api_key = "asdasds"
+    my_cse_id = "asdasdasd"
+    text = ""
+    query = text.replace("Cerca ", "")
+    self.AssistantAnswers.append("[" + current_time + "] Scegli tra:\n"
+                                                      "1) Google Search Results ""\n"
+                                                      "2) Youtube Links""\n"
+                                 )
+
+    if text == "1":
+        def google_search(search_term, api_key, cse_id, **kwargs):
+            service = build("customsearch", "v1", developerKey=api_key)
+            res = service.cse().list(q=search_term, cx=cse_id, **kwargs).execute()
+            return res
+
+        results = google_search(query, my_api_key, my_cse_id)
+
+        print(self.AssistantAnswers.append("[" + current_time + "]"
+                                                                "\n GoogleSearchResults \n"
+                                           ))
+        print(self.AssistantAnswers.append("[" + current_time + "]"
+                                                                "Title == " +
+                                           results['items'][0]['title']
+                                           ))
+        print(self.AssistantAnswers.append("[" + current_time + "]Link ==" +
+                                           results['items'][0]['link']
+                                           ))
+        snippet = results['items'][0]['snippet'].replace("\n", "")
+        html_snippet = results['items'][0]['htmlSnippet'].replace("\n", "")
+        html_snippet = html_snippet.replace("<b>", "")
+        html_snippet = html_snippet.replace("</b>", "")
+        html_snippet = html_snippet.replace("<br>", "")
+        html_snippet = html_snippet.replace("&nbsp;…", ".")
+        print(self.AssistantAnswers.append("[" + current_time + "]"
+                                                                "Description == " +
+                                           snippet +
+                                           html_snippet
+                                           ))
+        print("\n\n")
+
+
 class Ui_Assistant(object):
+    def __init__(self):
+        self.centralwidget = QtWidgets.QWidget(Assistant)
+        self.AssistantAnswers = QtWidgets.QTextEdit(self.centralwidget)
+        self.Questions = QtWidgets.QTextEdit(self.centralwidget)
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.Richiesta = QtWidgets.QLineEdit(self.centralwidget)
+        self.Submit = QtWidgets.QPushButton(self.centralwidget)
+        self.progressBar = QtWidgets.QProgressBar(self.centralwidget)
+        self.commandLinkButton = QtWidgets.QCommandLinkButton(self.centralwidget)
+        self.commandLinkButton_2 = QtWidgets.QCommandLinkButton(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(Assistant)
+        self.statusbar = QtWidgets.QStatusBar(Assistant)
+
     def setupUi(self, Assistant):
         Assistant.setObjectName("Assistant")
         Assistant.setDocumentMode(False)
@@ -30,58 +87,48 @@ class Ui_Assistant(object):
         Assistant.resize(550, 465)
         Assistant.setMaximumSize(550, 465)
         Assistant.setMinimumSize(550, 465)
-        self.centralwidget = QtWidgets.QWidget(Assistant)
         self.centralwidget.setObjectName("centralwidget")
 
-        self.AssistantAnswers = QtWidgets.QTextEdit(self.centralwidget)
         self.AssistantAnswers.setGeometry(QtCore.QRect(10, 10, 261, 301))
+
         self.AssistantAnswers.setObjectName("AssistantAnswers")
         self.AssistantAnswers.setReadOnly(True)
         self.AssistantAnswers.setText("Answers: \n")
 
-        self.Questions = QtWidgets.QTextEdit(self.centralwidget)
         self.Questions.setGeometry(QtCore.QRect(280, 10, 261, 301))
         self.Questions.setObjectName("Questions")
         self.Questions.setReadOnly(True)
         self.Questions.setText("Requests: \n")
 
-        self.label = QtWidgets.QLabel(self.centralwidget)
         self.label.setGeometry(QtCore.QRect(10, 320, 131, 16))
         self.label.setObjectName("label")
 
-        self.Richiesta = QtWidgets.QLineEdit(self.centralwidget)
         self.Richiesta.setGeometry(QtCore.QRect(10, 340, 371, 21))
         self.Richiesta.setText("")
         self.Richiesta.setObjectName("Richiesta")
         self.Richiesta.returnPressed.connect(lambda: self.richiesta(self.Richiesta.text()))
 
-        self.Submit = QtWidgets.QPushButton(self.centralwidget)
         self.Submit.setGeometry(QtCore.QRect(390, 340, 141, 23))
         self.Submit.setObjectName("Submit")
         self.Submit.clicked.connect(lambda: self.richiesta(self.Richiesta.text()))
 
-        self.progressBar = QtWidgets.QProgressBar(self.centralwidget)
         self.progressBar.setGeometry(QtCore.QRect(10, 370, 521, 16))
         self.progressBar.setProperty("value", 0)
         self.progressBar.setObjectName("progressBar")
 
-        self.commandLinkButton = QtWidgets.QCommandLinkButton(self.centralwidget)
         self.commandLinkButton.setGeometry(QtCore.QRect(10, 400, 131, 41))
         self.commandLinkButton.setObjectName("commandLinkButton")
         self.commandLinkButton.clicked.connect(assistance)
 
-        self.commandLinkButton_2 = QtWidgets.QCommandLinkButton(self.centralwidget)
         self.commandLinkButton_2.setGeometry(QtCore.QRect(400, 400, 141, 41))
         self.commandLinkButton_2.setObjectName("commandLinkButton_2")
         self.commandLinkButton_2.clicked.connect(close)
 
         Assistant.setCentralWidget(self.centralwidget)
-        self.menubar = QtWidgets.QMenuBar(Assistant)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 550, 21))
         self.menubar.setObjectName("menubar")
 
         Assistant.setMenuBar(self.menubar)
-        self.statusbar = QtWidgets.QStatusBar(Assistant)
         self.statusbar.setObjectName("statusbar")
         Assistant.setStatusBar(self.statusbar)
 
@@ -112,49 +159,8 @@ class Ui_Assistant(object):
                 tts("Ciao! Sono il tuo assistente personale. Scrivi help per una serie di aiuti.")
                 self.Richiesta.clear()
             elif search in text:
-                query = text.replace("Cerca ", "")
-                self.AssistantAnswers.append("[" + current_time + "] Scegli tra:\n"
-                                                                  "1) Google Search Results ""\n"
-                                                                  "2) Youtube Links""\n"
-                                             )
-                self.Richiesta.clear()
-                text = ""
-                while text != "1":
-                    text = self.Richiesta.text()
-                print("sas")
-                my_api_key = "AIzaSyB3GZwgnSJYUqFJLXCEY12vsISBAe2CIbw"
-                my_cse_id = "a1c10504d73c92fc4"
 
-                if text == "1":
-                    def google_search(search_term, api_key, cse_id, **kwargs):
-                        service = build("customsearch", "v1", developerKey=api_key)
-                        res = service.cse().list(q=search_term, cx=cse_id, **kwargs).execute()
-                        return res
-
-                    results = google_search(query, my_api_key, my_cse_id)
-
-                    print(self.AssistantAnswers.append("[" + current_time + "]"
-                                                                            "\n GoogleSearchResults \n"
-                                                       ))
-                    print(self.AssistantAnswers.append("[" + current_time + "]"
-                                                                            "Title == " +
-                                                       results['items'][0]['title']
-                                                       ))
-                    print(self.AssistantAnswers.append("[" + current_time + "]Link ==" +
-                                                       results['items'][0]['link']
-                                                       ))
-                    snippet = results['items'][0]['snippet'].replace("\n", "")
-                    html_snippet = results['items'][0]['htmlSnippet'].replace("\n", "")
-                    html_snippet = html_snippet.replace("<b>", "")
-                    html_snippet = html_snippet.replace("</b>", "")
-                    html_snippet = html_snippet.replace("<br>", "")
-                    html_snippet = html_snippet.replace("&nbsp;…", ".")
-                    print(self.AssistantAnswers.append("[" + current_time + "]"
-                                                                            "Description == " +
-                                                       snippet +
-                                                       html_snippet
-                                                       ))
-                    print("\n\n")
+                ScrapingRequest(self, text, current_time)
 
             elif text == "Help" or text == "help":
                 self.AssistantAnswers.append("[" + current_time + "] Per la funzione ricerca digitare: Cerca (es: "
